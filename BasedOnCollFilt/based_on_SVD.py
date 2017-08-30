241aee91dff6a9d1b1f31060127aa782ed4337f3
jose <pyprogrammer88@gmail.com>
2017-08-30 22:58:03 +0200

    adding reset

--- BasedOnCollFilt/based_on_SVD.py
+++ BasedOnCollFilt/based_on_SVD.py
@@ -5,10 +5,17 @@ from sklearn.decomposition import TruncatedSVD
 
 def based_on_svd(ratings_df, products_df, name, corr_value):
     """
-    1000 people with 50 products. We got 10000 recommendations so in average each person recommend 10 product
+    1000 people with 50 products. We got 10000 recommendations
+    so in average each person recommend 10 product
     """
     # Utility  1000x50  1000 people x 50 product
-    ratings_pivot = pd.pivot_table(data=ratings_df, values='rating', index='userID', columns='productID', fill_value=0)
+    ratings_pivot = pd.pivot_table(
+        data=ratings_df,
+        values='rating',
+        index='userID',
+        columns='productID',
+        fill_value=0
+    )
 
     # Transposing
     X = ratings_pivot.T  # 50x1000  50 product x 1000 people
@@ -17,13 +24,14 @@ def based_on_svd(ratings_df, products_df, name, corr_value):
     SVD = TruncatedSVD(n_components=12, random_state=17)
     decomposed_matrix = SVD.fit_transform(X)  # 50x12  50 products x 12 features
 
-    # correlation matrix
-    corr_mat = np.corrcoef(decomposed_matrix)  # we got 50x50, diagonal [[1, 0.2], [0.2, 1]]
+    # correlation matrix, we got 50x50, diagonal [[1, 0.2], [0.2, 1]]
+    corr_mat = np.corrcoef(decomposed_matrix)  #
 
     # get the product id input by user
     index_column = products_df[products_df['productName'] == name].index[0]
 
-    # get the correlation values of the product respect to the rest, corr of the product with itself = 1
+    # get the correlation values of the product respect to the rest,
+    # corr of the product with itself = 1
     product_corr_values = corr_mat[index_column]
 
     # mask values of dataframe with values of corr higher than....
--- BasedOnLogRegr/based_on_log_regr.py
+++ BasedOnLogRegr/based_on_log_regr.py
@@ -3,12 +3,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 
-def based_on_logreg(x, report=True):
+def based_on_logreg(x, report=False):
 
     product_featured = pd.read_csv('product_featured.csv')
 
-    X = product_featured.ix[:, :-1].values  # all columns but last as inputs features for model
-    y = product_featured.ix[:, -1].values  # last column to predict. input labels
+    # all columns but last as inputs features for model
+    X = product_featured.ix[:, :-1].values
+    # last column to predict. input labels
+    y = product_featured.ix[:, -1].values
 
     x = x.values  # what we want to classified
 
@@ -17,7 +19,7 @@ def based_on_logreg(x, report=True):
     y_pred = model_trained.predict(x)
 
     if report:
-        Y_pred = model_trained.predict(X)  # we predict Y for X(real), and then we compare with y(real)
+        Y_pred = model_trained.predict(X)
         print(classification_report(y, Y_pred))
 
     return y_pred
@@ -28,7 +30,8 @@ def main():
     new_client = pd.read_csv('product.csv')
 
     y_pred = based_on_logreg(new_client, report=True)
-    print("Is this person valid for this Product? {}".format("Yes" if y_pred == 1 else "No"))
+    print("Is this person valid for this "
+          "Product? {}".format("Yes" if y_pred == 1 else "No"))
 
 if __name__ == '__main__':
     main()
